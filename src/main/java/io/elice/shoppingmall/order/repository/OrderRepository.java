6120f2aa4883c3ef298085b9dab2f269f48c3a06
Sinorang <sinholang@naver.com>
2024-05-31 23:40:00 +0900

    feat: reposiotry 생성 - repository 패키지 생성 - OrderRepository 구현

---
+++ src/main/java/io/elice/shoppingmall/order/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package io.elice.shoppingmall.order.repository;
+
+import io.elice.shoppingmall.order.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
