d450142af374661d9a680073da26a1402b9eefa6
narim <ehdrkd648@naver.com>
2024-06-03 17:24:06 +0900

    Revert "feat: entity 추가"
    This reverts commit c38f20a3f2ac46817cf7196a59fb92d387b08aca.
    
    s#

--- src/main/java/io/elice/shoppingmall/cart/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package io.elice.shoppingmall.cart.repository;
-
-import io.elice.shoppingmall.cart.domain.cart.Entity.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-
-}
