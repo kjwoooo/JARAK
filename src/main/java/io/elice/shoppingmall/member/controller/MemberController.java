8eae355932222b4466d4457a121281bd71d11c3f
FFFace <59927358+FFFace@users.noreply.github.com>
2024-06-03 14:58:30 +0900

    feat: 멤버 조회 추가

--- src/main/java/io/elice/shoppingmall/member/controller/MemberController.java
+++ src/main/java/io/elice/shoppingmall/member/controller/MemberController.java
@@ -3,10 +3,12 @@ package io.elice.shoppingmall.member.controller;
 import io.elice.shoppingmall.member.entity.LoginInfo;
 import io.elice.shoppingmall.member.entity.Member;
 import io.elice.shoppingmall.member.entity.MemberDTO;
+import io.elice.shoppingmall.member.entity.MemberResponseDTO;
 import io.elice.shoppingmall.member.service.MemberService;
 import io.elice.shoppingmall.security.SecurityConfig;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -14,6 +16,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import io.elice.shoppingmall.security.JwtTokenUtil;
 
@@ -22,6 +25,29 @@ import io.elice.shoppingmall.security.JwtTokenUtil;
 public class MemberController {
 
     private final MemberService memberService;
+
+    @GetMapping("/members")
+    public ResponseEntity getMembers(){
+        List<MemberResponseDTO> members = memberService.findAll().stream().map(
+            MemberResponseDTO::new).toList();
+
+        if(members.isEmpty()){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(members, HttpStatus.OK);
+    }
+
+    @GetMapping("/members/{id}")
+    public ResponseEntity getMember(@RequestParam Long id){
+        Member member = memberService.findById(id).orElse(null);
+
+        if(member == null){
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity(new MemberResponseDTO(member), HttpStatus.OK);
+    }
 
     @PostMapping("/loginTest")
     public ResponseEntity login(@RequestBody LoginInfo loginInfo, HttpServletResponse response){
