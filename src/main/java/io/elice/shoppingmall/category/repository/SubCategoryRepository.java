d6575ee40b6fa3858c7e2c4de4b1a6dd9ae14fa6
[Cloud3] 권재우 <kjw1111k@naver.com>
2024-06-03 17:30:40 +0900

    refactor: @Repository 어노테이션 추가

--- src/main/java/io/elice/shoppingmall/category/repository/SubCategoryRepository.java
+++ src/main/java/io/elice/shoppingmall/category/repository/SubCategoryRepository.java
@@ -2,7 +2,9 @@ package io.elice.shoppingmall.category.repository;
 
 import io.elice.shoppingmall.category.entity.SubCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SubCategoryRepository extends JpaRepository<SubCategory, Long> {
 
 }
