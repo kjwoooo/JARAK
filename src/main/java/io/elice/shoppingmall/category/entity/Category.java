bfac409aae291a8c3cde842024297ec156717a63
[Cloud3] 권재우 <kjw1111k@naver.com>
2024-06-07 17:46:27 +0900

    feat: 메인 카테고리와 서브 카테고리 계층형 구조로 변환

--- src/main/java/io/elice/shoppingmall/category/entity/Category.java
+++ src/main/java/io/elice/shoppingmall/category/entity/Category.java
@@ -1,5 +1,6 @@
 package io.elice.shoppingmall.category.entity;
 
+import io.elice.shoppingmall.category.dto.CategoryDto;
 import io.elice.shoppingmall.entity.baseEntity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -9,10 +10,12 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * 상의, 하의, 신발, 악세사리 등 <p>
- * 상위 카테고리에 대한 entity
+ * 상위 카테고리에 대한 하위 카테고리를<p>
+ * 계층형 구조로 나타냄
  * */
 @Entity
 @Getter @Setter
@@ -27,6 +30,19 @@ public class Category extends BaseEntity {
     @Column(nullable = false)
     private String name;
 
-    @OneToMany(mappedBy = "category")
-    private List<SubCategory> subCategories = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> subCategories = new ArrayList<>();
+
+    // Entity를 DTO로 변환하는 메서드
+    public CategoryDto entityToDto(Category category) {
+        return new CategoryDto(category.getName(),
+                Optional.ofNullable(category.getParent())
+                        .map(Category::getId)
+                        .orElse(null));
+    }
+
 }
