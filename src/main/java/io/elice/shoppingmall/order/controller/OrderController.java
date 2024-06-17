b6e8bc70d581b28c0c13f38199c4b146e0b57e54
Sinorang <sinholang@naver.com>
2024-06-17 12:20:28 +0900

    fix: 피드백 내용 반영
    - OrderController에서 인증된 사용자를 가져오는 로직을 변경
    - @AuthenticationPrincipal 사용하여 인증된 사용자를 가져옴
    - SecurityConfig에서 orders/ 경로로 시작하는 api는 일반회원만 접근
      가능하도록 접근 권한 설정

--- src/main/java/io/elice/shoppingmall/order/controller/OrderController.java
+++ src/main/java/io/elice/shoppingmall/order/controller/OrderController.java
@@ -9,7 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,60 +33,64 @@ public class OrderController {
         this.memberService = memberService;
     }
 
-    // 회원을 JWT 토큰으로부터 검색
-    //Feedback : 인증이 끝난 부분으로 가져와야지 jwt를 여기서 가져와서 인증 로직을 여기서 처리하면 안됩니다.
-    private Member getMemberFromJwtToken(String jwtToken) {
-        return memberService.findByJwtToken(jwtToken);
+    private Member getAuthenticatedMember(UserDetails userDetails) {
+        return memberService.findByUsername(userDetails.getUsername());
     }
 
     // 주문 내역 조회
     @GetMapping
-    public ResponseEntity<Page<OrderDTO>> getOrders(@CookieValue String jwtToken,
+    public ResponseEntity<Page<OrderDTO>> getOrders(@AuthenticationPrincipal UserDetails userDetails,
                                                     @RequestParam(defaultValue = "0") int page,
                                                     @RequestParam(defaultValue = "10") int size) {
-        Member member = getMemberFromJwtToken(jwtToken);
+        Member member = getAuthenticatedMember(userDetails);
         Page<OrderDTO> orders = orderService.getOrdersByMemberId(member.getId(), page, size);
         return new ResponseEntity<>(orders, HttpStatus.OK);
     }
 
     // 주문 상세 내역 조회
     @GetMapping("/order/{orderId}")
-    public ResponseEntity<OrderDTO> getOrderDetails(@CookieValue String jwtToken,
+    public ResponseEntity<OrderDTO> getOrderDetails(@AuthenticationPrincipal UserDetails userDetails,
                                                     @PathVariable Long orderId) {
-        Member member = getMemberFromJwtToken(jwtToken);
+        Member member = getAuthenticatedMember(userDetails);
         OrderDTO orderDetails = orderService.getOrderDetailsByOrderId(orderId, member.getId());
         return new ResponseEntity<>(orderDetails, HttpStatus.OK);
     }
 
     // 주문 생성
     @PostMapping
-    public ResponseEntity<OrderDTO> createOrder(@CookieValue String jwtToken, @Valid @RequestBody OrderDTO orderDTO) {
-        OrderDTO createdOrder = orderService.createOrder(jwtToken, orderDTO);
+    public ResponseEntity<OrderDTO> createOrder(@AuthenticationPrincipal UserDetails userDetails,
+                                                @Valid @RequestBody OrderDTO orderDTO) {
+        Member member = getAuthenticatedMember(userDetails);
+        OrderDTO createdOrder = orderService.createOrder(member, orderDTO);
         return new ResponseEntity<>(createdOrder, HttpStatus.CREATED);
     }
 
     // 주문 수정 페이지 호출
     @GetMapping("/{orderId}")
-    public ResponseEntity<OrderDTO> getUpdateOrderPage(@CookieValue String jwtToken, @PathVariable Long orderId) {
-        Member member = getMemberFromJwtToken(jwtToken);
+    public ResponseEntity<OrderDTO> getUpdateOrderPage(@AuthenticationPrincipal UserDetails userDetails,
+                                                       @PathVariable Long orderId) {
+        Member member = getAuthenticatedMember(userDetails);
         OrderDTO orderDTO = orderService.getUpdateOrderPage(orderId, member.getId());
         return new ResponseEntity<>(orderDTO, HttpStatus.OK);
     }
 
     // 주문 수정
     @PutMapping("/{orderId}")
-    public ResponseEntity<OrderDTO> updateOrder(@CookieValue String jwtToken, @PathVariable Long orderId,
+    public ResponseEntity<OrderDTO> updateOrder(@AuthenticationPrincipal UserDetails userDetails,
+                                                @PathVariable Long orderId,
                                                 @Valid @RequestBody OrderDTO orderDTO) {
-        OrderDTO updatedOrder = orderService.updateOrder(jwtToken, orderId, orderDTO);
+        Member member = getAuthenticatedMember(userDetails);
+        OrderDTO updatedOrder = orderService.updateOrder(member, orderId, orderDTO);
         return new ResponseEntity<>(updatedOrder, HttpStatus.OK);
     }
 
     // 주문 취소(환불)
     @DeleteMapping("/{orderId}")
-    public ResponseEntity<String> deleteOrder(@CookieValue String jwtToken,
+    public ResponseEntity<String> deleteOrder(@AuthenticationPrincipal UserDetails userDetails,
                                               @PathVariable Long orderId,
                                               @RequestParam(required = false) String refundReason) {
-        orderService.cancelOrder(jwtToken, orderId, refundReason);
+        Member member = getAuthenticatedMember(userDetails);
+        orderService.cancelOrder(member, orderId, refundReason);
         return new ResponseEntity<>("주문이 성공적으로 취소되었습니다.", HttpStatus.OK);
     }
 }
